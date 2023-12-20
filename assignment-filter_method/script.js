b70f6b814b45987ef99962507204a2ca00eaddfa
chaithra0604 <148523341+chaithra0604@users.noreply.github.com>
2023-12-20 11:02:49 +0530

    Add files via upload

---
+++ assignment-filter_method/script.js
@@ -0,0 +1,19 @@
+//odd
+
+let number;
+const ar = [1,2,3,4,5,6,7,8,9,10];
+const odd = ar.filter(number => {
+    return number % 2!==0
+});
+console.log(odd);
+
+
+//even
+
+let num;
+const arr = [1,2,3,4,5,6,7,8,9,10];
+const even = arr.filter(num => {
+    return num % 2==0
+});
+console.log(even); 
+
